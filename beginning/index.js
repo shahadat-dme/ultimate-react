9352f25b2ae7c52b04ca8fd36a137576afa0c29d
Shahadat <shahadat2cp17@gmail.com>
2022-01-16 21:41:38 +0600

    beginning

--- beginning/index.js
+++ beginning/index.js
@@ -1,4 +1,9 @@
 /*
+ReactDOM.render(<h1>Hello, everyone! I am here.</h1>,
+    document.getElementById("root"));
+*/
+
+/*
 ReactDOM.render(<h1>Hello, everyone! I am here.<p>I love React</p></h1>,
     document.getElementById("root"));
 */
@@ -22,8 +27,16 @@ ReactDOM.render(
     document.getElementById("root"));
 */
 
-
+/*
 const h1 =document.createElement("h1")
 h1.textContent="Hello BD"
 h1.className="header"
 document.getElementById("root").append(h1);
+*/
+
+
+const element = <h1 className>This is JSX</h1>
+console.log(element)
+
+ReactDOM.render(element,
+    document.getElementById("root"));
---
+++ beginning/navbar.js
@@ -0,0 +1,13 @@
+const navbar = (
+    <nav>
+        <h1>Shahadat's</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+ 
+ReactDOM.render(navbar, document.getElementById("root"))
+
