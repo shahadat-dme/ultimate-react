b2044c8ea692b6344eab3e43c0197e8bbcb75f3c
Shahadat <shahadat2cp17@gmail.com>
2022-01-16 19:26:15 +0600

    ultimate React

---
+++ index.js
@@ -0,0 +1,9 @@
+/*
+ReactDOM.render(<h1>Hello, everyone! I am here.<p>I love React</p></h1>,
+    document.getElementById("root"));
+*/
+
+// ReactDOM.render(<h1>I am Shahadat</h1>,document.querySelector("#root"))
+
+ReactDOM.render(<ul><li>BD</li><li>USA</li></ul>,
+    document.getElementById("root"));
