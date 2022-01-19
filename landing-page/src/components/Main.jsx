5241b431736b7b43ab240995b5e28a1e2117a82e
Shahadat <shahadat2cp17@gmail.com>
2022-01-19 23:54:24 +0600

    section 1 completed

--- landing-page/src/components/Main.jsx
+++ landing-page/src/components/Main.jsx
@@ -3,15 +3,16 @@ import './Main.css'
 
 export default function Main() {
     return (
-        <div className="container">
+        <main className="container">
             <h1 className="main--title">Fun facts about React</h1>
             <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100K stars on GitHub</li>
                 <li>Is maintained by Facebook</li>
+                <li>React is a free and open-source front-end JavaScript library</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </div>
+        </main>
     )
 }
